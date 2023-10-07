f8129af2751cd4cc8f91d14a0f43348c31d457bc
emman-pip <parrenoemmanuel756@gmail.com>
2023-10-07 17:16:51 +0800

    Add works and credentials placeholders.

--- src/index.js
+++ src/index.js
@@ -8,7 +8,19 @@ function blurBackground(target) {
   });
 }
 
+function slowScroll(element, target) {
+  element.onscroll = () => {
+    const scrolltoTop = document.scrollingElement.scrollTop;
+
+    const xvalue = "center";
+    const scrollFactor = 0.3;
+    const yValue = scrolltoTop * scrollFactor;
+    target.style.backgroundPosition = xvalue + " " + yValue + "px";
+  };
+}
+
 (() => {
+  slowScroll(document.querySelector("body"), document.querySelector("main"));
   blurBackground(".heroPage");
   blurBackground(".bottomNavBar");
 })();
